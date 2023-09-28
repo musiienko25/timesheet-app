3d70373723c675cbaad455641a2574938ef1d619
musiienko25 <mysterious240@gmail.com>
2023-09-28 16:00:24 +0300

    added sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./App.scss";
 import { EmployeeProvider } from "./EmployeeContext";
 import EmployeeTable from "./components/EmployeeTable";
 
